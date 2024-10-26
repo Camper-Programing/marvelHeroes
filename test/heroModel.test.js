01e61ea2c1a6250a70b2a2d507cc33fe867dfafc
saselinee <146088867+saselinee@users.noreply.github.com>
2024-10-26 10:30:36 +0200

    .

---
+++ test/heroModel.test.js
@@ -0,0 +1,39 @@
+const { expect } = require('chai');
+const HeroModel = require('../models/heroModel');
+
+describe('HeroModel', () => {
+    let model;
+
+    before(() => {
+        model = new HeroModel();
+    });
+
+    it('should fetch all heroes', () => {
+        const heroes = model.getAllHeroes();
+        expect(heroes).to.be.an('array');
+        expect(heroes).to.have.lengthOf.at.least(1);
+    });
+
+    it('should get a hero by ID', () => {
+        const hero = model.getHeroById(1);
+        expect(hero).to.be.an('object');
+        expect(hero).to.have.property('name', 'Captain America');
+    });
+
+    it('should create a new hero', () => {
+        const newHero = { name: 'Black Widow', alias: 'Natasha Romanoff', powers: ['Martial arts', 'Espionage'] };
+        const createdHero = model.createHero(newHero);
+        expect(createdHero).to.have.property('id');
+        expect(createdHero.name).to.equal('Black Widow');
+    });
+
+    it('should update an existing hero', () => {
+        const updatedHero = model.updateHero(1, { alias: 'Steve Rogers (updated)' });
+        expect(updatedHero).to.have.property('alias', 'Steve Rogers (updated)');
+    });
+
+    it('should delete a hero', () => {
+        const deletedHero = model.deleteHero(1);
+        expect(deletedHero[0]).to.have.property('name', 'Captain America');
+    });
+});
